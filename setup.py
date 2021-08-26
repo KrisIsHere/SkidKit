a3f32cdb16d57b8578cf5facfe52ec5baf38df4f
KrisIsHere <73352269+KrisIsHere@users.noreply.github.com>
2021-08-26 14:20:48 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import sys
 from sys import platform
 
 if os.getuid():
-    print("Script By KrisIsHere | Setup Tool Built 1.0")
+    print("Script By KrisIsHere | Setup Tool Built 1.1")
     exit('\nroot access required\n')
 
 if platform == "linux" or platform == "linux2":
@@ -13,25 +13,19 @@ if platform == "linux" or platform == "linux2":
     lmao = input("Choose your package manager(1-apt 2-pacman 3-xbps): ")
     if lmao == "1":
             xd = input("Would you like to proceed with the instalation?: (Y/N) ")
-            if xd == "y":
-                os.system("apt install php tcpdump traceroute scapy python3-pip ; pip3 install bane pysocks ; mkdir tools/.info ; touch tools/.info/setup.py")
-            elif xd == "Y":
+            if xd in ["Y", "y"]:
                 os.system("apt install php tcpdump traceroute scapy python3-pip ; pip3 install bane pysocks ; mkdir tools/.info ; touch tools/.info/setup.py")
             else:
                 print("Setup stopped.")
     if lmao == "2":
             xd = input("Would you like to proceed with the instalation?: (Y/N) ")
-            if xd == "y":
+            if xd in ["Y", "y"]:
                 os.system("pacman -S php tcpdump scapy traceroute traceroute python3-pip ; pip3 install bane pysocks ; mkdir tools/.info ; touch tools/.info/setup.py")
-            elif xd == "Y":
-                os.system("pacman -S php tcpdump scapy traceroute python3-pip ; pip3 install bane pysocks ; mkdir tools/.info ; touch tools/.info/setup.py")
             else:
                 print("Setup stopped.")
     if lmao == "3":
             xd = input("Would you like to proceed with the instalation?: (Y/N) ")
-            if xd == "y":
-                os.system("xbps-install -S php tcpdump scapy traceroute python3-pip ; pip3 install bane pysocks ; mkdir tools/.info ; touch tools/.info/setup.py")
-            elif xd == "Y":
+            if xd in ["Y", "y"]:
                 os.system("xbps-install -S php tcpdump scapy traceroute python3-pip ; pip3 install bane pysocks ; mkdir tools/.info ; touch tools/.info/setup.py")
             else:
                 print("Setup stopped.")
