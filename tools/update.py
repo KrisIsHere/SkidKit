26bee89aeaabdff7ffc52036a1d7dff30e913416
KrisIsHere <73352269+KrisIsHere@users.noreply.github.com>
2021-08-26 14:29:19 +0300

    Update update.py

--- tools/update.py
+++ tools/update.py
@@ -10,7 +10,7 @@ if xd in ["Y", "y"]:
     os.system("rm -rf SkidKit ; git clone https://github.com/krisishere/SkidKit ; cp -r .info " + parent + "/SkidKit/tools/")
     os.system("rm -rf .info")
     os.chdir(parent + "/SkidKit")
-    os.system("python3 main.py")
+    os.system("python3 skidkit.py")
     exit()
 else:
     print("ok")
