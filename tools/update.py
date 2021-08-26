a8b934408ba404ba2ce3ee873c7bc8db1cfcf4e0
KrisIsHere <73352269+KrisIsHere@users.noreply.github.com>
2021-08-26 14:21:33 +0300

    Update update.py

--- tools/update.py
+++ tools/update.py
@@ -5,15 +5,9 @@ path = os.getcwd()
 parent = os.path.abspath(os.path.join(path, os.pardir))
 os.chdir(parent)
 print(os.getcwd())
-print("SkidKit update tool made by KrisIsHere | ver. 1.0")
+print("SkidKit update tool made by KrisIsHere | ver. 1.1")
 xd = input("Would you like to update? (Y/N): ")
-if xd == "Y":
-    os.system("rm -rf SkidKit ; git clone https://github.com/krisishere/SkidKit ; cp -r .info " + parent + "/SkidKit/tools/")
-    os.system("rm -rf .info")
-    os.chdir(parent + "/SkidKit")
-    os.system("python3 main.py")
-    sys.exit()
-if xd == "y":
+if xd in ["Y", "y"]:
     os.system("rm -rf SkidKit ; git clone https://github.com/krisishere/SkidKit ; cp -r .info " + parent + "/SkidKit/tools/")
     os.system("rm -rf .info")
     os.chdir(parent + "/SkidKit")
